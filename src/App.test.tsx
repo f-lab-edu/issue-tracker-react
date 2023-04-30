e9badbb15311de606f2afdb70103d1a951b27425
bytrustu <bytrustu@gmail.com>
2023-05-01 01:19:07 +0900

    chore: package module 추가 및 jest 설정

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import App from './App';
-
-test('App 컴포넌트를 테스트합니다.', () => {
-  render(<App />);
-  const element = screen.getByText(/이슈 트래커 리액트/i);
-  expect(element).toBeInTheDocument();
-});
