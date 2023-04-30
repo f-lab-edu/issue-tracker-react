6f338fb57c355423aa69d45db8f73c514530d706
bytrustu <bytrustu@gmail.com>
2023-05-01 01:35:22 +0900

    feat: Layout 컴포넌트 추가

---
+++ src/ui/components/layout/index.ts
@@ -0,0 +1,3 @@
+export { default as PageHeader } from './PageHeader';
+export { default as PageLayout } from './PageLayout';
+export { default as PageBody } from './PageBody';
---
+++ src/ui/components/layout/layout.css.ts
@@ -0,0 +1,44 @@
+import { style } from '@vanilla-extract/css';
+import styleToken from '@ui/core/styleToken.css';
+
+const pageLayout = style({
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh',
+  minWidth: '1600px',
+});
+
+const pageHeaderContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  height: styleToken.size.headerHeight,
+  padding: '0 48px',
+  backgroundColor: styleToken.color.orange['700'],
+});
+
+const pageHeaderLogo = style({
+  height: '100%',
+  width: 'auto',
+});
+
+const pageBody = style({
+  display: 'flex',
+  flexDirection: 'column',
+  height: styleToken.size.bodyHeight,
+  padding: 64,
+  backgroundColor: styleToken.color.background,
+});
+
+export const pageLayoutStyle = {
+  container: pageLayout,
+};
+
+export const pageHeaderStyle = {
+  container: pageHeaderContainer,
+  logo: pageHeaderLogo,
+};
+
+export const pageBodyStyle = {
+  container: pageBody,
+};
