b485d17e6e595d6257ae79beb4b09a0c7a6c961f
bytrustu <bytrustu@gmail.com>
2023-05-07 18:35:40 +0900

    feat: useStore 추가

---
+++ src/lib/store/storeUtils.ts
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from 'react';
+import BaseStore from '@lib/store/config/baseStore';
+
+const storeUtils = <State, Action>(store: BaseStore<State, Action>) => {
+  const stateRef = useRef(store.getState());
+  const [, forceRender] = useState({});
+
+  const selector = <SelectedState>(selector: (state: State) => SelectedState): SelectedState => selector(stateRef.current);
+
+  const dispatch = (action: Action) => {
+    store.dispatch(action);
+    stateRef.current = store.getState();
+    forceRender({});
+  };
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      stateRef.current = store.getState();
+      forceRender({});
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [store]);
+
+  return {
+    selector,
+    dispatch,
+  };
+};
+
+export default storeUtils;
