cad50187434350ccf00ef3092fb1c461bca42c3b
bytrustu <bytrustu@gmail.com>
2023-05-11 03:34:03 +0900

    feat: 보드 아이템 삭제 기능 추가

--- src/constant/message.ts
+++ src/constant/message.ts
@@ -1,2 +1,3 @@
 export const EMPTY_BOARD_MESSAGE = '내용을 입력해주세요.';
 export const DELETE_BOARD_COLUMN_MESSAGE = '컬럼을 삭제하시겠습니까?';
+export const DELETE_BOARD_ITEM_MESSAGE = '아이템을 삭제하시겠습니까?';
--- src/ui/components/board/BoardItem.tsx
+++ src/ui/components/board/BoardItem.tsx
@@ -4,31 +4,49 @@ import styleToken from '@ui/core/styleToken.css';
 import { IconButton } from '@ui/components/button';
 import { boardItemStyle } from '@ui/components/board/board.css';
 import { BoardItemType } from '@lib/type/board.type';
+import { useConfirm } from '@lib/hooks';
+import { apiDeleteBoardItem } from '@lib/api/board.ts';
+import useStore from '@lib/store/useStore.ts';
+import { removeBoardItemAction } from '@lib/store/reducer/boardReducer.ts';
+import { DELETE_BOARD_ITEM_MESSAGE } from '@/constant/message.ts';
 
-type Props = {} & BoardItemType;
+type Props = {
+  boardId: string;
+} & BoardItemType;
 
-const BoardItem = ({ itemId, title, author }: Props) => (
-  <HStack className={boardItemStyle.container} data-id={itemId} draggable>
-    <VStack className={boardItemStyle.contentHeader}>
-      <MdEditCalendar color={styleToken.color.gray600} size={18} />
-    </VStack>
-    <VStack className={boardItemStyle.contentBody}>
-      <div>{title}</div>
-      <div>
-        <span>added by</span>
-        <strong className={boardItemStyle.author}>{author}</strong>
-      </div>
-    </VStack>
-    <HStack className={boardItemStyle.contentFooter}>
-      <IconButton
-        icon="close"
-        onClick={() => {}}
-        style={{
-          flex: 'unset',
-        }}
-      />
+const BoardItem = ({ boardId, itemId, title, author }: Props) => {
+  const showConfirm = useConfirm();
+  const { dispatch } = useStore();
+  const handleDelete = async () => {
+    const confirm = await showConfirm(DELETE_BOARD_ITEM_MESSAGE, { enableBackdropClick: true });
+    if (confirm) {
+      await apiDeleteBoardItem({ boardId, itemId });
+      dispatch(removeBoardItemAction({ boardId, itemId }));
+    }
+  };
+  return (
+    <HStack className={boardItemStyle.container} data-id={itemId} draggable>
+      <VStack className={boardItemStyle.contentHeader}>
+        <MdEditCalendar color={styleToken.color.gray600} size={18} />
+      </VStack>
+      <VStack className={boardItemStyle.contentBody}>
+        <div>{title}</div>
+        <div>
+          <span>added by</span>
+          <strong className={boardItemStyle.author}>{author}</strong>
+        </div>
+      </VStack>
+      <HStack className={boardItemStyle.contentFooter}>
+        <IconButton
+          icon="close"
+          onClick={handleDelete}
+          style={{
+            flex: 'unset',
+          }}
+        />
+      </HStack>
     </HStack>
-  </HStack>
-);
+  );
+};
 
 export default BoardItem;
