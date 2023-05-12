f54072e08cd7d6202729e2611bc6db6e3a25593a
bytrustu <bytrustu@gmail.com>
2023-05-12 20:55:57 +0900

    feat: BoardHeader 컬럼삭제, 컬럼 이름 변경 기능 추가

--- src/ui/components/board/BoardHeader.tsx
+++ src/ui/components/board/BoardHeader.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import { HStack, VStack } from '@ui/components/common';
 import { BoardTitle, BoardItemCreate } from '@ui/components/board';
 import useStore from '@lib/store/useStore.ts';
-import { apiDeleteBoardColumn, apiPostBoard } from '@lib/api/board.ts';
-import { addBoardItemAction, removeBoardColumnAction } from '@lib/store/reducer/boardReducer.ts';
-import { useAlert, useConfirm } from '@lib/hooks';
+import { apiDeleteBoardColumn, apiPatchBoardColumn, apiPostBoard } from '@lib/api/board.ts';
+import { addBoardItemAction, editBoardColumnAction, removeBoardColumnAction } from '@lib/store/reducer/boardReducer.ts';
+import { useAlert, useConfirm, useModal } from '@lib/hooks';
+import { TextModal } from '@ui/components/dialog';
 import { IconButton } from '../button';
 import { DELETE_BOARD_COLUMN_MESSAGE, EMPTY_BOARD_MESSAGE } from '@/constant/message.ts';
 
@@ -18,6 +19,7 @@ const BoardHeader = ({ title = '', count = 0, boardId }: Props) => {
   const { dispatch } = useStore();
   const showAlert = useAlert();
   const showConfirm = useConfirm();
+  const showModal = useModal();
   const [isTextOpen, setIsTextOpen] = useState(false);
 
   const handleOpenText = () => {
@@ -46,9 +48,17 @@ const BoardHeader = ({ title = '', count = 0, boardId }: Props) => {
     }
   };
 
+  const handleEditBoardColumnDbClick = async () => {
+    const columnText = await showModal(<TextModal title="이슈 콘텐츠 컬럼 수정" type="textarea" value={title} />);
+    if (typeof columnText === 'string') {
+      await apiPatchBoardColumn({ boardId, title: columnText });
+      dispatch(editBoardColumnAction({ boardId, title: columnText }));
+    }
+  };
+
   return (
     <VStack>
-      <HStack style={{ justifyContent: 'space-between' }}>
+      <HStack style={{ justifyContent: 'space-between' }} onDoubleClick={handleEditBoardColumnDbClick}>
         <BoardTitle title={title} count={count} />
         <HStack>
           <IconButton icon="add" onClick={handleOpenText} />
